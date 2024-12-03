bd074e4e7755d8eb0fc6b05ea6e2f492ee2a4334
alinmaslov55 <maslovalin55.com>
2024-12-03 20:29:29 +0200

    creat functie de verificare a nume file

--- headers.cpp
+++ headers.cpp
@@ -5,3 +5,14 @@ const int ANCHOR = 8;
 void newline(){
     std::cout << "\n";
 }
+
+const std::string extract_filename(std::string line){
+    int length = line.length();
+    if(length < 5 || line.at(0) == '.') return "";
+    std::string termination = line.substr(length - 4, length);
+    if(termination ==".txt"){
+        return line;
+    } else {
+        return line + ".txt";
+    }
+}
--- headers.hpp
+++ headers.hpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
 
 extern const int ANCHOR;
 
 void newline();
+
+const std::string extract_filename(std::string line);
 
 #endif
