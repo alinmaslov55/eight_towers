4b0d8e3df54c1481772c131570b860b1c0bd983d
alinmaslov55 <maslovalin55.com>
2024-12-24 12:24:24 +0200

    corrected window and button sizes

--- src/headers.cpp
+++ src/headers.cpp
@@ -5,13 +5,13 @@ extern const float BOARD_X = 100.0f;
 extern const float BOARD_Y = 200.0f;
 extern const float BOARD_SIZE = 441.0f;
 extern const float CELL_SIZE = 50.0f;
-extern const int WINDOW_WIDTH = 600;
+extern const int WINDOW_WIDTH = 641;
 extern const int WINDOW_HEIGHT = 700;
 extern const int PARTS = 5;
 extern const float GAP_SIZE = 3.0f;
 extern const float BIG_GAP_SIZE = 10.0f;
-extern const float BUTTON_WIDTH = 200.0f;
-extern const float BUTTON_HEIGHT = 100.0f;
+extern const float BUTTON_WIDTH = 441.0f;
+extern const float BUTTON_HEIGHT = 50.0f;
 extern const float BUTTON_X = 100.0f;
 extern const float BUTTON_Y = 100.0f;
 
